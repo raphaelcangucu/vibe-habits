cd9b00642b3d07f97cd374d45ed21001b0aad26f
Raphael <me@raphaelcangucu.com>
2025-10-05 21:21:02 -0300

    Initial Commit

---
+++ habits trackerTests/habits_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  habits_trackerTests.swift
+//  habits trackerTests
+//
+//  Created by Raphael Canguçu on 05/10/25.
+//
+
+import Testing
+@testable import habits_tracker
+
+struct habits_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
